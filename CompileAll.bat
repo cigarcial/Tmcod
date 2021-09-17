bc18a0af4c0dbd8acde8663b59d238b6396aae13
Ciro Garcia <cigarcial@unal.edu.co>
2021-09-17 18:07:47 -0500

    Limpieza 2

---
+++ CompileAll.bat
@@ -0,0 +1,12 @@
+coqc Defs_Tactics.v -Q . Tmcod
+coqc Defs_Proposition.v -Q . Tmcod
+coqc Defs_Process.v -Q . Tmcod
+coqc Defs_Typing.v -Q . Tmcod
+
+coqc Facts_FVars.v -Q . Tmcod
+coqc Facts_Names.v -Q . Tmcod
+coqc Props_Propositions.v -Q . Tmcod
+
+coqc Facts_Process.v -Q . Tmcod
+coqc Facts_MOpen.v -Q . Tmcod
+coqc Props_Process.v -Q . Tmcod
